424f30b7821204851eae020e22eb4fcbef146f1b
Md Aqib <contact2aqib@gmail.com>
2025-03-18 23:43:06 +0530

    Glimpse of API in Node

--- project-01/index.js
+++ project-01/index.js
@@ -4,5 +4,42 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
+const users = require("./data/USERS.json")
+
+app.get("/users", (req, res) => {
+  return res.json(users);
+})
+
+app.get("/users/:id", (req, res) => {
+  const userId = Number(req.params.id);
+  user = users.find((user) => user.id == userId);
+  return res.json(`You have requested for user: ${ user.first_name } ${ user.last_name }`);
+})
+
+app.delete("/users/:id", (req, res) => {
+  return res.json({ status: "Pending" });
+})
+
+app.patch("/users/:id", (req, res) => {
+  return res.json({ status: "Pending" });
+})
+
+// As we can see above the route is repeating:
+// So we can do this -
+
+/*
+app
+  .route("/users/:id")
+  .get((req, res) => {
+
+  })
+  .patch((req, res) => {
+
+  })
+  .delete((req, res) => {
+
+  })
+*/
+
 app.listen(PORT, () => console.log(`Server started on ${ PORT } port.`))
 
